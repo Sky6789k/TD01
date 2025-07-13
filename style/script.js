b545d32b93ff89a21717ba0d8a110574f1282092
khánh <sp.khanhnk@gmail.com>
2025-07-13 15:27:20 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -8,10 +8,10 @@ const sound3 = document.getElementById('bg-music3');
 const sound4 = document.getElementById('bg-music4');
 
 const content = [
-  { gif: 'https://i.pinimg.com/originals/7a/ef/73/7aef734a86dce4dc206976d4f0586f2c.gif', message: 'Bạn chắc chứ? 😢' },
+  { gif: 'https://i.pinimg.com/originals/7a/ef/73/7aef734a86dce4dc206976d4f0586f2c.gif', message: 'Dương ơi chắc chưa? 😢' },
   { gif: 'https://i.pinimg.com/originals/c8/07/e2/c807e26d8aed392f172f0bf441f60626.gif', message: 'Thử nghĩ lại nha 🥺' },
   { gif: 'https://i.pinimg.com/originals/0d/ac/7e/0dac7e14010362ff081e2167be218341.gif', message: 'Đừng mà, cho tớ cơ hội đi 💔' },
-  { gif: 'https://i.pinimg.com/originals/88/e7/86/88e786492cc527584feee199936813dd.gif', message: 'Thiệt luôn đó hả? 😭' },
+  { gif: 'https://i.pinimg.com/originals/88/e7/86/88e786492cc527584feee199936813dd.gif', message: 'Thiệt luôn hả Mai Ka? 😭' },
   { gif: 'https://i.pinimg.com/originals/82/be/ae/82beaeb21c686871437f88bbc1593288.gif', message: 'Một lần nữa thôi, năn nỉ đó 😞' },
   { gif: 'https://i.pinimg.com/originals/97/91/de/9791de11497556c4a5e800427c48fc47.gif', message: 'Tớ buồn đó nha... 😔' },
 ];
@@ -25,9 +25,9 @@ noBtn.addEventListener('click', () => {
   clickCount++;
 
   if (clickCount === 3) {
-    noBtn.textContent = 'Bấm Có đi 😭';
+    noBtn.textContent = 'Bấm Có đi Mai Ka😭';
   } else if (clickCount === 7) {
-    noBtn.textContent = 'Năn nỉ đó bấm Có đi 😭';
+    noBtn.textContent = 'Năn nỉ đó bấm Có đi mà 😭';
   }
 
   if (clickCount <= 5) {
@@ -60,7 +60,7 @@ noBtn.addEventListener('click', () => {
 });
 
 yesBtn.addEventListener('click', () => {
-  question.textContent = 'Tớ biết mà! Tớ cũng thích cậu nhiều lắm ❤️';
+  question.textContent = 'Tớ biết mà! Tớ cũng thích cậu nhiều lắm Mai Ka❤️';
   gif.src = 'https://i.pinimg.com/originals/7e/f6/9c/7ef69cd0a6b0b78526c8ce983b3296fc.gif';
   noBtn.style.display = 'none';
   yesBtn.style.display = 'none';
